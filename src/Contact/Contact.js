d82287849979dd193d820a6cda0959266620f8b7
Santiago <beloquisantiago@gmail.com>
2019-11-19 11:40:14 -0300

    Contact stars animation

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -3,16 +3,8 @@ import './Contact.css';
 
 const Contact = ()=>{         
     let points = [];
-    let counter = 0;
-    let maxSize = 100;
-    let opacity = 0;
-    let canvasOpacity = 1;
-    let goRight = true;
-    let goDown = true;
     useEffect(()=>{
         const canvas = document.getElementById('contact-canvas');
-        // canvas.width = window.innerWidth * 0.45;
-        // canvas.height = window.innerHeight * 0.5; 
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight; 
         const context = canvas.getContext('2d');
@@ -20,11 +12,10 @@ const Contact = ()=>{
         for(let i=0; i < 500; i++){
             points.push(
                 {
-                    initialX: Math.random() * ((canvas.width / 2 + 20) - (canvas.width / 2 - 20)) + (canvas.width / 2 - 20),
-                    initialY : Math.random() * ((canvas.height / 2 + 20) - (canvas.height / 2 - 20)) + (canvas.height / 2 - 20),
-                    xGoRight: Math.random() > 0.5,
-                    yGoDown: Math.random() > 0.5
-
+                    x: Math.random() * canvas.width,
+                    y: Math.random() * canvas.height,
+                    velocity: 0.03,
+                    radius: 0.3
                 }
             )
         }
@@ -36,17 +27,20 @@ const Contact = ()=>{
         context.clearRect(0,0,canvas.width, canvas.height);   
         for(let i = 0; i < points.length; i++){
             context.beginPath();
-            context.arc(points[i]['initialX'], points[i]['initialY'], 2, 0, Math.PI * 2, false);
+            context.arc(points[i]['x'], points[i]['y'], points[i]['radius'], 0, Math.PI * 2, false);
             context.fillStyle = '#e5e500';
             context.fill();
         }
         for(let point of points){
-            if(point.initialX >= canvas.width || point.initialX <= 0 || point.initialY >= canvas.height || point.initialY <= 0){
-                point.initialX = canvas.width / 2;
-                point.initialY = canvas.height / 2;
+            if(point.x >= canvas.width || point.x <= 0 || point.y >= canvas.height || point.y <= 0){
+                point.x = Math.random() * canvas.width;
+                point.y = Math.random() * canvas.height;
+                point.radius = 0.05;
             }
-            point.initialX = point.xGoRight? point.initialX + Math.random() * 50: point.initialX - Math.random() * 50;
-            point.initialY = point.yGoDown? point.initialY + Math.random() * 50: point.initialY - Math.random() * 50;
+
+            point.x =  point.x +  (point.x - canvas.width / 2) * point.velocity;
+            point.y = point.y + (point.y - canvas.height / 2) * point.velocity;
+            point.radius = point.radius < 5? point.radius += point.velocity: point.radius;
             
         }
         setTimeout(()=>{
